be13dbfebc1e835224fb858f0ffc18a7dc2078e4
sarka92 <sarki@172.20.46.158>
2016-04-11 17:33:33 +0200

    Uso de replace temp with query

--- RefactorizacionFowler/src/refactorizacionFowler/Customer.java
+++ RefactorizacionFowler/src/refactorizacionFowler/Customer.java
@@ -37,8 +37,8 @@ public class Customer {
 				frequentRenterPoints++;
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+					+ String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
