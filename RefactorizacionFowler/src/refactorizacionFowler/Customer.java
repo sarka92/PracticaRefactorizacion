9cf92b5ce3aeae1e27b10b82fb34cee324724d38
sarka92 <sarki@172.20.46.158>
2016-04-11 17:52:54 +0200

    Inclusion de htmlstatement

--- RefactorizacionFowler/src/refactorizacionFowler/Customer.java
+++ RefactorizacionFowler/src/refactorizacionFowler/Customer.java
@@ -37,12 +37,12 @@ public class Customer {
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		result += "You earned " + String.valueOf(getFrequentRenterPoints())
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
 				+ " frequent renter points";
 		return result;
 	}
 
-	private int getFrequentRenterPoints() {
+	private int getTotalFrequentRenterPoints() {
 		int result = 0;
 		 Enumeration rentals = _rentals.elements();
 		 while (rentals.hasMoreElements()) {
@@ -61,4 +61,23 @@ public class Customer {
 		}
 		return result;
 	}
+	
+	public String htmlStatement() {
+		 Enumeration rentals = _rentals.elements();
+		 String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		 while (rentals.hasMoreElements()) {
+		 Rental each = (Rental) rentals.nextElement();
+		
+		 //show figures for each rental
+		 result += each.getMovie().getTitle()+ ": " +
+		 String.valueOf(each.getCharge()) + "<BR>\n";
+		 }
+		 //add footer lines
+		 result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) +
+		"</EM><P>\n";
+		 result += "On this rental you earned <EM>" +
+		 String.valueOf(getTotalFrequentRenterPoints()) +
+		 "</EM> frequent renter points<P>";
+		 return result;
+		 }
 }
